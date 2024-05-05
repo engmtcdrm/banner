9ceaedc286e093a8a9830e39e082890effe9ea89
engmtcdrm <deedsoftheforsaken@gmail.com>
2024-05-05 15:17:20 -0400

    Updated tests

--- tests/test_proem.py
+++ tests/test_proem.py
@@ -2,12 +2,13 @@ from contextlib import nullcontext as does_not_raise
 import os
 
 import pytest
+from colorama import Fore
 
 # from contextlib import nullcontext as does_not_raise
 from src.proem import Proem
 
 @pytest.mark.parametrize('app_nm, flavor_text, version, repo_url, width, border_char, border_color, description, expected_build_text, expected_except', [
-    ("test-app", None, None, None, 1, '#', 'magenta', None, '', pytest.raises(AssertionError)),
+    ("test-app", None, None, None, 1, '#', 'magenta', None, f'{Fore.MAGENTA}############{Fore.RESET}\n{Fore.MAGENTA}#{Fore.RESET} test-app {Fore.MAGENTA}#{Fore.RESET}\n{Fore.MAGENTA}############{Fore.RESET}\n', does_not_raise()),
 ])
 
 def test_build_func(app_nm, flavor_text, version, repo_url, width, border_char, border_color, description, expected_build_text, expected_except):
@@ -25,5 +26,8 @@ def test_build_func(app_nm, flavor_text, version, repo_url, width, border_char, 
         )
 
         build_text = proem.build()
+
+        print(build_text)
+        print(expected_build_text)
 
         assert build_text == expected_build_text
