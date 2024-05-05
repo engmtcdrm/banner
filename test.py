490f9f0952d7e64ebe7ad96c9ef6880ebf5815fb
engmtcdrm <deedsoftheforsaken@gmail.com>
2024-05-05 15:17:26 -0400

    Updated

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 from src.proem import Proem
 
-p = Proem(app_nm = 'test-app', flavor_text='this is my flavor', version='v1.0.0')
+p = Proem(
+    app_nm = 'test-app',
+    flavor_text='this is my flavor',
+    version='v1.0.0',
+    repo_url='https://github.com/test-app',
+    width=-1
+)
 
 print(p.build())
-print(p._find_max_width())
